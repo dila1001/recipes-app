5eea0a765b1abff5899f410bbe504c5788f64dbb
Adila Razmi <adilarazmi@Adilas-MBP.localdomain>
2022-06-09 10:46:06 +0200

    Fix bug and add resolver to recipe route

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,22 +7,29 @@ import { Recipe } from './recipe.model';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Nasi Lemak',
-      'Rich coconut rice + sweet and spicy sambal',
-      'https://www.thespruceeats.com/thmb/eZF07lmCjlFfCVbzIzrsG22emMo=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/nasi-lemak-coconut-pandan-rice-3030254-step-07-ccbeb81d2b754d9c899b2edfaf386bc9.jpg',
-      [new Ingredient('Onion', 2), new Ingredient('Coconut milk', 1)]
-    ),
-    new Recipe(
-      'Chicken Biryani',
-      'Spiced chicken steam cooked with saffron rice',
-      'https://i.ytimg.com/vi/0SEZn3aP5JM/maxresdefault.jpg',
-      [new Ingredient('Cilantro', 2), new Ingredient('Yoghurt', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Nasi Lemak',
+  //     'Rich coconut rice + sweet and spicy sambal',
+  //     'https://www.thespruceeats.com/thmb/eZF07lmCjlFfCVbzIzrsG22emMo=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/nasi-lemak-coconut-pandan-rice-3030254-step-07-ccbeb81d2b754d9c899b2edfaf386bc9.jpg',
+  //     [new Ingredient('Onion', 2), new Ingredient('Coconut milk', 1)]
+  //   ),
+  //   new Recipe(
+  //     'Chicken Biryani',
+  //     'Spiced chicken steam cooked with saffron rice',
+  //     'https://i.ytimg.com/vi/0SEZn3aP5JM/maxresdefault.jpg',
+  //     [new Ingredient('Cilantro', 2), new Ingredient('Yoghurt', 1)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
@@ -19,5 +21,25 @@ export class DataStorageService {
       .subscribe((response) => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://recipe-app-f9b5a-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipesService.setRecipes(recipes);
+        })
+      );
   }
 }
